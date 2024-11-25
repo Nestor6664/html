3de84b21c999179c88c8d50834930a4087e6deff
Nestor6664 <nestorsenkov@gmail.com>
2024-11-25 18:49:46 +0200

    UPD

--- homework15/assets/js/dist/main.dev.js
+++ homework15/assets/js/dist/main.dev.js
@@ -19,4 +19,33 @@ console.log("\u0406\u043C'\u044F: ".concat(firstName, ", \u041F\u0440\u0456\u043
 // let last-name = "Нестор"; // 
 // let first name = "Нестор"; // 
 // let let = "Нестор";       // 
-// let @name = "Нестор";     //
+// let @name = "Нестор";     // 
+
+var radius = parseFloat(prompt("Введіть радіус кола  в метрах:"));
+
+if (!isNaN(radius) && radius > 0) {
+  var area = Math.PI * Math.pow(radius, 2);
+  alert("\u041F\u043B\u043E\u0449\u0430 \u0437 \u0440\u0430\u0434\u0456\u0443\u0441\u043E\u043C ".concat(radius, " \u0434\u043E\u0440\u0456\u0432\u043D\u044E\u0454 ").concat(area.toFixed(2), "."));
+} else {
+  alert("введіть правильне число для радіуса.");
+}
+
+var distance = parseFloat(prompt("Введіть відстань між містами в кілометрах:"));
+var time = parseFloat(prompt("За скільки годин ви хочете дістатися?"));
+
+if (!isNaN(distance) && distance > 0 && !isNaN(time) && time > 0) {
+  var speed = distance / time;
+  alert("\u0429\u043E\u0431 \u0434\u0456\u0441\u0442\u0430\u0442\u0438\u0441\u044F \u0437\u0430 ".concat(time, " \u0433\u043E\u0434\u0438\u043D, \u043F\u043E\u0442\u0440\u0456\u0431\u043D\u043E \u0457\u0445\u0430\u0442\u0438 \u0437\u0456 \u0448\u0432\u0438\u0434\u043A\u0456\u0441\u0442\u044E ").concat(speed.toFixed(2), " \u043A\u043C/\u0433\u043E\u0434."));
+} else {
+  alert("введіть коректні додатні числа для відстані та часу.");
+}
+
+var exchangeRate = 0.94;
+var dollars = parseFloat(prompt("Введіть суму в доларах:"));
+
+if (!isNaN(dollars) && dollars >= 0) {
+  var euros = dollars * exchangeRate;
+  alert("".concat(dollars, " \u0434\u043E\u043B\u0430\u0440\u0456\u0432 \u0434\u043E\u0440\u0456\u0432\u043D\u044E\u0454 ").concat(euros.toFixed(2), " \u0454\u0432\u0440\u043E \u0437\u0430 \u043A\u0443\u0440\u0441\u043E\u043C ").concat(exchangeRate, "."));
+} else {
+  alert("Будь ласка, введіть коректну суму в доларах.");
+}
--- homework15/assets/js/main.js
+++ homework15/assets/js/main.js
@@ -1,27 +1,55 @@
-const userName = prompt ("Enter your name")
- // alert("Hello, " + userName + "!")
+const userName = prompt("Enter your name")
+// alert("Hello, " + userName + "!")
 alert(`Hello, ${userName}`)
 
 const currentYear = 2024
 const birthYear = prompt("Enter your birth year")
 const userAge = currentYear - birthYear
-alert (`Your age is: ${userAge} years`)
+alert(`Your age is: ${userAge} years`)
 
-const width = prompt ("Enter square side width")
+const width = prompt("Enter square side width")
 const perim = width * 4
 alert("Perimetr is " + perim)
 
 console.log(`Ім'я: ${firstName}, Прізвище: ${lastName}`);
 
 
-        // Однорядковий коментар
-        /* Багаторядковий
-           коментар */
+// Однорядковий коментар
+/* Багаторядковий
+   коментар */
 
 
-  // Неправильні імена 
-        // let 1name = "Нестор";   // 
-        // let last-name = "Нестор"; // 
-        // let first name = "Нестор"; // 
-        // let let = "Нестор";       // 
-        // let @name = "Нестор";     // 
+// Неправильні імена 
+// let 1name = "Нестор";   // 
+// let last-name = "Нестор"; // 
+// let first name = "Нестор"; // 
+// let let = "Нестор";       // 
+// let @name = "Нестор";     // 
+
+let radius = parseFloat(prompt("Введіть радіус кола  в метрах:"));
+if (!isNaN(radius) && radius > 0) {
+      let area = Math.PI * Math.pow(radius, 2);
+      alert(`Площа з радіусом ${radius} дорівнює ${area.toFixed(2)}.`);
+} else {
+      alert("введіть правильне число для радіуса.");
+}
+
+let distance = parseFloat(prompt("Введіть відстань між містами в кілометрах:"));
+let time = parseFloat(prompt("За скільки годин ви хочете дістатися?"));
+if (!isNaN(distance) && distance > 0 && !isNaN(time) && time > 0) {
+      let speed = distance / time;
+      alert(`Щоб дістатися за ${time} годин, потрібно їхати зі швидкістю ${speed.toFixed(2)} км/год.`);
+} else {
+      alert("введіть коректні додатні числа для відстані та часу.");
+}
+
+
+const exchangeRate = 0.94; 
+let dollars = parseFloat(prompt("Введіть суму в доларах:"));
+if (!isNaN(dollars) && dollars >= 0) {
+      let euros = dollars * exchangeRate;
+      alert(`${dollars} доларів дорівнює ${euros.toFixed(2)} євро за курсом ${exchangeRate}.`);
+} else {
+      alert("Будь ласка, введіть коректну суму в доларах.");
+}
+
